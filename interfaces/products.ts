e5873ec9a2dec3cb9f695f9ee5a85568cd96a131
Salvatore Difranco <salvatoredifranco8@gmail.com>
2023-03-30 15:28:12 +0200

    feat: wrote custom hooks to get data

---
+++ interfaces/products.ts
@@ -0,0 +1,20 @@
+export type ProductsResponse ={
+  limit: number;
+  products: Product[];
+  skip: number;
+  total: number;
+}
+
+export type Product = {
+  'id': number;
+  'title': string;
+  'description': string;
+  'price': number;
+  'discountPercentage': number;
+  'rating': number;
+  'stock': number;
+  'brand': string;
+  'category': string;
+  'thumbnail': string;
+  'images': string[];
+};
