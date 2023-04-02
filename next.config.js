b51c45ca420e49a63e3aa859880af07a97eeef6a
Salvatore Difranco <salvatoredifranco8@gmail.com>
2023-04-02 13:25:24 +0200

    chore: config import svg

--- next.config.js
+++ next.config.js
@@ -1,13 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /*
   eslint-disable
   @typescript-eslint/no-var-requires,
   global-require
 */
+const withImages = require('next-images');
 
 module.exports = () => {
   const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
   });
 
-  return withBundleAnalyzer();
+  return withBundleAnalyzer({
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
+
+      return config;
+    },
+  });
 };
